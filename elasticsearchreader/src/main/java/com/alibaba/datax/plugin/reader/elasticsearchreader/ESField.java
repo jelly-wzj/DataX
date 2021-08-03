998a8f92bcc2708e2e8e660f780ee4c4f901afac
jelly <haode888@163.com>
2021-08-03 14:19:01 +0800

    add elasticsearch reader plugin

---
+++ elasticsearchreader/src/main/java/com/alibaba/datax/plugin/reader/elasticsearchreader/ESField.java
@@ -0,0 +1,61 @@
+package com.alibaba.datax.plugin.reader.elasticsearchreader;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.List;
+
+public class ESField {
+    private String name;
+    private String alias;
+    private Object value;
+    private List<ESField> child;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<ESField> getChild() {
+        return child;
+    }
+
+    public void setChild(List<ESField> child) {
+        this.child = child;
+    }
+
+    public boolean hasChild() {
+        return child != null && !child.isEmpty();
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public String getFinalName(String nameCase) {
+        if (StringUtils.isNotBlank(alias)) {
+            return alias;
+        } else if (StringUtils.isBlank(name)) {
+            return "";
+        } else if ("UPPERCASE".equalsIgnoreCase(nameCase)) {
+            return name.toUpperCase();
+        } else if ("LOWERCASE".equalsIgnoreCase(nameCase)) {
+            return name.toLowerCase();
+        }
+        return name;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
+}
